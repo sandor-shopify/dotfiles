f49ba3523c6b4fb69cf4c5ed08abfcb7f6932b5f
Sandor Balo <sandor.balo@shopify.com>
2021-08-23 15:33:17 -0400

    updated install.sh to include vscode extensions

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-echo "starting dotfile" > dotfile.log
+echo "-------- STARTING DOTFILE --------\n" > dotfile.log
+
+for extension_name in \
+ eamodio.gitlens \
+ dbaeumer.vscode-eslint; do
+    code --install-extension $extension_name >> dotfile.log
+done
+
+echo "\n-------- DONE DOTFILE --------" >> dotfile.log
