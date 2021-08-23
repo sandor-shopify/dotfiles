bd9e558c38becee065caf4faba6fc568ba38eb5b
Sandor Balo <sandor.balo@shopify.com>
2021-08-23 16:02:33 -0400

    removed vscode extensions because they are not needed

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 echo "-------- STARTING DOTFILE --------\n" > dotfile.log
 
-for extension_name in \
- eamodio.gitlens \
- dbaeumer.vscode-eslint; do
-    code --install-extension $extension_name >> dotfile.log
-done
-
 echo "\n-------- DONE DOTFILE --------" >> dotfile.log
